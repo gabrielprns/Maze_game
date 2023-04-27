8dd0832dfeea30ee5e7464afbe9e21c74b2a33b7
Caio Vieira <caiovieiraprofissional@protonmail.com>
2023-04-27 01:46:37 -0300

    Spawn melhorado

--- src/scripts/Level.gd
+++ src/scripts/Level.gd
@@ -5,19 +5,18 @@ onready var enemy = $Enemy;
 onready var navmesh = $NavigationMeshInstance;
 onready var map = $NavigationMeshInstance/Map;
 
+var prepared = false;
+
 func _physics_process(delta):
 	var loc = player.global_transform.origin;
 	enemy.call_deferred("set_target", loc);
+
 
 func _ready():
 	var player_pos = map.start_room;
 	var enemy_pos = map.end_room;
 	
-	player.global_transform.origin = player_pos;
+	#player.global_transform.origin = player_pos;
 	enemy.global_transform.origin = enemy_pos;
-
-
-func _on_Map_generated():
-	pass
-	#print("hi")
-	#navmesh.bake_navigation_mesh();
+	
+	navmesh.bake_navigation_mesh(false);
